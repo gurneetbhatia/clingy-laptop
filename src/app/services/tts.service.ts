d99389b0c500bb70a5091b7d943cc5afdc4de084
Finn Hudson <11finnh@gmail.com>
2020-09-19 17:01:44 +0100

    feat: url generation service

---
+++ src/app/services/tts.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TtsService {
+
+  constructor() { }
+
+  playText(text: string): void {
+    
+  }
+}
