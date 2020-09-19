69985c5415d3d1f61f37b97826563a8ce941e2cc
Gurneet Bhatia <sbgurneet@gmail.com>
2020-09-19 12:21:23 +0100

    feat: implemented placeholder funcs

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
   signInWithGitHub(): void {
-    
+    this.loginService.signInWithGitHub();
   }
 
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class LoginService {
 
   constructor() { }
+
+  signInWithGitHub(): void {
+    console.log("placeholder function for logging in with github");
+  }
 }
