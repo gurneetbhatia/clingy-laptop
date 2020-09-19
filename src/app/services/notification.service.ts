47cff3c0cf656450596801fd7bcfd81988591471
Gurneet Bhatia <sbgurneet@gmail.com>
2020-09-19 15:06:43 +0100

    feat: created notif service

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
+
+  showSuccess(message: string): void {
+    this.toastr.success(message);
+  }
+
+  showError(message: string): void {
+    this.toastr.error(message);
+  }
 }
